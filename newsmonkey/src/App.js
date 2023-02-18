76df5ff45c237cc6101ee0741531d4770e60750c
abhiyadav7470 <108442826+abhiyadav7470@users.noreply.github.com>
2023-02-18 14:55:45 +0530

    updates

--- newsmonkey/src/App.js
+++ newsmonkey/src/App.js
@@ -2,12 +2,28 @@ import './App.css';
 import Apicall from './components/Apicall';
 import Articlecart from './components/Articlecart';
 import Navbar from './components/Navbar'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const[product, setproduct]=useState({
+    product:[]
+  });
+
+  useEffect(()=>{axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=8cd8e6dc58e84bc1a718ed806eb7311d`).then((response)=>{
+      setproduct({...product,product: response.data.articles});
+  },err=>{
+      console.log(err);
+  })
+
+  },[])
   return (
     <div className="App">
+            <div className='conta'>
       <Navbar/>
-      <Apicall/>
+      <Articlecart productsl={product.product}/>
+
+      </div>
     </div>
   );
 }
--- newsmonkey/src/components/Apicall.js
+++ newsmonkey/src/components/Apicall.js
@@ -1,21 +1,16 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Articlecart from './Articlecart';
 
 function Apicall(){
-    const[product, setproduct]=useState();
-
-        axios.get(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=8cd8e6dc58e84bc1a718ed806eb7311d`).then((response)=>{
-            setproduct(response.data.articles);
-            console.log(product)
-        },err=>{
-            console.log(err);
-        })
+   
+        
+    
        
    
         return(
             <>
-                <Articlecart products={product}/>
+                <Articlecart />
             </>
         )
   
